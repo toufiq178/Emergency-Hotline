cdc702c80c05fd4cf4de10efdf9f5e9b42bb25cf
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-03-20 00:31:16 +0600

    history section made dynamic live time add & copy btn click to copy text

--- script.js
+++ script.js
@@ -4,15 +4,10 @@ let copyTotalCount = 0;
 let heartTotalCount = 0;
 let totalCoins = 99;
 
-
-
 const heartCount = document.getElementById("heart-count");
 const allCopyCount = document.getElementById("all-copy-count");
 const coins = document.getElementById("coins");
 const historyContainer = document.getElementById("history-container");
-
-
-
 
 document.querySelectorAll(".heart-icon").forEach((item) => {
   // console.log(item);
@@ -33,13 +28,7 @@ document.querySelectorAll(".heart-icon").forEach((item) => {
   });
 });
 
-document.querySelectorAll(".copy-btn").forEach((item) => {
-  item.addEventListener("click", function () {
-    copyTotalCount++;
 
-    allCopyCount.innerText = `${copyTotalCount} copy`;
-  });
-});
 
 // document.querySelectorAll(".call-btn").forEach((callBtn) => {
 //   callBtn.addEventListener("click", function () {
@@ -63,11 +52,44 @@ document
   .addEventListener("click", function (e) {
     console.log();
 
+    const card = e.target.closest(".card");
+
+    const serviceName = card.querySelector(".service-name").innerText;
+    const serviceNumber = card.querySelector(".service-number").innerText;
+    // console.log(card , serviceName);
+
+    console.log(serviceName);
+
+    if (e.target.classList.contains("copy-btn")) {
+      console.log(allCopyCount);
+
+      if (copyTotalCount == 3) {
+        alert("you have copy already 3 services");
+        return;
+      }
+
+      copyTotalCount++;
+
+      allCopyCount.innerText = `${copyTotalCount} copy`;
+
+      navigator.clipboard
+        .writeText(serviceName)
+        .then(() => {
+          console.log("Copied:", serviceName);
+        })
+        .catch((err) => {
+          console.error("Failed to copy:", err);
+        });
+    }
+
+    // const serviceNumber = e.target.classList.contains("service-number").innerText;
+
+    // console.log(serviceName , "click sevice name" , serviceNumber , "click service number");
+
     if (e.target.classList.contains("call-btn")) {
       coins.innerHTML = "";
       console.log("call btn click");
 
-      
       if (totalCoins < 20) {
         alert("you need more coins to call");
 
@@ -78,17 +100,29 @@ document
       totalCoins = totalCoins - 20;
       coins.innerHTML = totalCoins;
 
-      displayCallHistory()
+      displayCallHistory(serviceName, serviceNumber);
     }
-
-
-
   });
 
+function displayCallHistory(serviceName, serviceNumber) {
+  const div = document.createElement("div");
 
+  const now = new Date();
+  const time = now.toLocaleTimeString();
 
-
-  function displayCallHistory() {
+  console.log(time);
+  div.innerHTML = `
+    <div class="flex justify-between items-center bg-gray-100 p-5 rounded-2xl">
+                            <div>
+                                <h2 class="text-xl  ">${serviceName}</h2>
+                                <p class="text-lg text-gray-600 ">${serviceNumber}</p>
+                            </div>
+                            <div>
+                                <p class=" text-xl">${time}</p>
+                            </div>
+                        </div>
     
-    historyContainer.innerHTML = ""
-  }
+    `;
+
+  historyContainer.appendChild(div);
+}
