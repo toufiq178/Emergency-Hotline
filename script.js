9ca69346fdacb0c0262f35da9fced24983860236
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-03-19 23:37:53 +0600

    call btn click coins managment added

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ console.log("hello js");
 
 let copyTotalCount = 0;
 let heartTotalCount = 0;
+let totalCoins = 99;
+
+
 
 const heartCount = document.getElementById("heart-count");
 const allCopyCount = document.getElementById("all-copy-count");
+const coins = document.getElementById("coins");
+const historyContainer = document.getElementById("history-container");
 
 
 
@@ -13,33 +18,77 @@ document.querySelectorAll(".heart-icon").forEach((item) => {
   // console.log(item);
 
   item.addEventListener("click", function () {
-
-
-    
-
     // isLiked = !isLiked
     if (item.classList.contains("liked")) {
-
-      count--;
-      item.classList.remove("liked" , 'fa-solid', "text-red");
-      item.classList.add("fa-regular")
-      
+      heartTotalCount--;
+      item.classList.remove("liked", "fa-solid", "text-red");
+      item.classList.add("fa-regular");
     } else {
-        count++;
-        item.classList.add("liked", "fa-solid" , "text-red");
-        item.classList.remove("fa-regular")
+      heartTotalCount++;
+      item.classList.add("liked", "fa-solid", "text-red");
+      item.classList.remove("fa-regular");
     }
 
     heartCount.innerText = heartTotalCount;
   });
 });
 
-document.querySelectorAll(".copy-btn").forEach(item => {
+document.querySelectorAll(".copy-btn").forEach((item) => {
+  item.addEventListener("click", function () {
+    copyTotalCount++;
 
-    item.addEventListener("click" , function () {
-        
-        copyTotalCount++
+    allCopyCount.innerText = `${copyTotalCount} copy`;
+  });
+});
 
-        allCopyCount.innerText = `${copyTotalCount} copy`
-    })
-})
+// document.querySelectorAll(".call-btn").forEach((callBtn) => {
+//   callBtn.addEventListener("click", function () {
+//     coins.innerHTML = "";
+//     console.log("call btn click");
+
+//     if (totalCoins < 20) {
+//       alert("you need more coins to call");
+
+//       coins.innerHTML = totalCoins;
+//       return;
+//     }
+
+//     totalCoins = totalCoins - 20;
+//     coins.innerHTML = totalCoins;
+//   });
+// });
+
+document
+  .getElementById("card-container")
+  .addEventListener("click", function (e) {
+    console.log();
+
+    if (e.target.classList.contains("call-btn")) {
+      coins.innerHTML = "";
+      console.log("call btn click");
+
+      
+      if (totalCoins < 20) {
+        alert("you need more coins to call");
+
+        coins.innerHTML = totalCoins;
+        return;
+      }
+
+      totalCoins = totalCoins - 20;
+      coins.innerHTML = totalCoins;
+
+      displayCallHistory()
+    }
+
+
+
+  });
+
+
+
+
+  function displayCallHistory() {
+    
+    historyContainer.innerHTML = ""
+  }
